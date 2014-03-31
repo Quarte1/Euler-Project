206c634294870b70b75fc67a546ef78d84a4032b
ungsik yun <ungsik.yun@gmail.com>
2014-03-31 21:56:37 +0900

    add problem 3 description

---
+++ src/3.py
@@ -0,0 +1,16 @@
+'''
+Created on 2014. 3. 31.
+
+Problem 3
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+
+@author: Ungsik Yun
+'''
+
+
+def get_lagerst_prime_factpr(target):
+    return target
+
+print get_lagerst_prime_factpr(13195)
