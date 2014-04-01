9946d91ed85cd36fc119e10d06493f974b6dcbf4
ungsik yun <ungsik.yun@gmail.com>
2014-04-01 17:34:26 +0900

    write some test code

---
+++ src/Prime.py
@@ -0,0 +1,62 @@
+'''
+Created on 2014. 3. 31.
+
+Find prime numbers
+
+@author: Ungsik Yun
+'''
+
+class Prime(object):
+
+    def __init__(self):
+        self.prime_list = [2,3]
+
+    def list_under(self, limit):
+    
+        for i in range(3, limit+1, 2):
+            if self.is_prime(i):
+                if i not in self.prime_list :
+                    self.prime_list.append(i)
+        result = []
+        for j in self.prime_list:
+            if j < limit:
+                result.append(j)
+            else:
+                break
+        return result
+        
+        
+    def is_prime(self, i):
+        if i == 2:
+            return True 
+        else :
+            for prime in self.prime_list:
+                if i % prime == 0 :
+                    return False
+        return True
+    
+    
+    def biggest_under(self, limit):
+        primes = self.list_under(limit)
+        return primes[-1]
+    
+    
+    def get_by(self, stop):
+        target = self.prime_list[-1]
+        while len(self.prime_list) < stop :
+            if self.is_prime(target) :
+                self.prime_list.append(target)
+            else:
+                target += 1
+        
+        return self.prime_list[0:stop]
+    
+    
+    def next_prime(self, ):
+        pass
+    
+    
+if __name__ == "__main__":
+    prime = Prime()
+    print prime.get_by(10000)
+    print len(prime.prime_list)
