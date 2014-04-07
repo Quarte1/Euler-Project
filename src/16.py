e7c7b24621fadffd8704b5c78805c82dc34a4faa
ungsik yun <ungsik.yun@gmail.com>
2014-04-07 14:43:53 +0900

    added problem description for 10

--- src/16.py
+++ src/16.py
@@ -1,3 +1,17 @@
+'''
+Created on 2014. 4. 3.
+
+Power digit sum
+Problem 16
+215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+
+Solution: 1366
+
+@author: Ungsik Yun
+'''
+
 i = 2**1000
 s = str(i)
 sum = 0
