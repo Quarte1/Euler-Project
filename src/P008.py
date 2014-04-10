47dc4f84ffb0d776fdba9ddfb75bdbc81664042f
ungsik yun <ungsik.yun@gmail.com>
2014-04-10 18:49:03 +0900

    refactor problem 8

--- src/P008.py
+++ src/P008.py
@@ -1,4 +1,14 @@
-q = "73167176531330624919225119674426574742355349194934\
+'''
+Largest product in a series
+Problem 8
+Find the greatest product of five consecutive digits in the 1000-digit number.
+
+@author: Ungsik Yun
+'''
+
+if __name__ == '__main__':
+
+    q = "73167176531330624919225119674426574742355349194934\
 96983520312774506326239578318016984801869478851843\
 85861560789112949495459501737958331952853208805511\
 12540698747158523863050715693290963295227443043557\
@@ -19,10 +29,10 @@ q = "73167176531330624919225119674426574742355349194934\
 05886116467109405077541002256983155200055935729725\
 71636269561882670428252483600823257530420752963450"
 
-max = 0
-for i in range(len(q)):
-	if i < (len(q)-5):
-		val = int(q[i])*int(q[i+1])*int(q[i+2])*int(q[i+3])*int(q[i+4])
-		if val > max:
-			max = val
-print max
+    result = 0
+    for i in range(len(q)):
+        if i < (len(q) - 5):
+            val = int(q[i]) * int(q[i + 1]) * int(q[i + 2]) * int(q[i + 3]) * int(q[i + 4])
+            if val > result:
+                result = val
+    print result
