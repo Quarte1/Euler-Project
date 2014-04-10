2b0f9165436de39c897436297b4c746f5154d018
ungsik yun <ungsik.yun@gmail.com>
2014-04-10 17:58:38 +0900

    refactor problem4

--- src/P004.py
+++ src/P004.py
@@ -13,30 +13,33 @@ Solution: 906609
 @author: Ungsik Yun
 '''
 
+
 def is_palindrome(i):
     stringed = str(i)
-    for i in range(len(stringed)/2):
-        if stringed[i] != stringed[-(i+1)]:
+    for i in range(len(stringed) / 2):
+        if stringed[i] != stringed[-(i + 1)]:
             return False
             break
     return True
 
+
 def is_palindrome_improved(i):
     return str(i) == str(i)[::-1]
-        
+
+
 def get_largest_2_3digit_multifled_pelindrome():
     pelindrome_list = []
 
-    i, j = 999,999
+    i, j = 999, 999
     while i > 100:
         j = 999
         while j > 100:
 #             print i,j
-            if is_palindrome_improved(i*j):
-                pelindrome_list.append(i*j)
+            if is_palindrome_improved(i * j):
+                pelindrome_list.append(i * j)
             j -= 1
         i -= 1
     return max(pelindrome_list)
-                      
+
 if __name__ == '__main__':
     print get_largest_2_3digit_multifled_pelindrome()
