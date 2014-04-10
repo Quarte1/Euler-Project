05bc52de414f6eb80fbe8d7846f109ae9bbff051
ungsik yun <ungsik.yun@gmail.com>
2014-04-10 18:58:53 +0900

    refactor problem 16

--- src/P016.py
+++ src/P016.py
@@ -1,20 +1,20 @@
 '''
 Created on 2014. 4. 3.
 
-Power digit sum
+Power digit result
 Problem 16
-215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+215 = 32768 and the result of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
-What is the sum of the digits of the number 2^1000?
+What is the result of the digits of the number 2^1000?
 
 Solution: 1366
 
 @author: Ungsik Yun
 '''
-
-i = 2**1000
-s = str(i)
-sum = 0
-for j in s:
-    sum += int(j)
-print sum
+if __name__ == '__main__':
+    i = 2 ** 1000
+    s = str(i)
+    result = 0
+    for j in s:
+        result += int(j)
+    print result
